1304de7e3163742907bb3bee36ad53e5ab7b2579
rosemary ledwaba <rosemaryledwaba@gmail.com>
2024-08-16 22:23:12 +0200

    Added tyle to main content

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,3 +1,11 @@
-import { MainContent } from './MainContent.1';
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f4f4f4', textAlign: 'left' }}>
+      <p>"style", "I love to visit New York, Paris, and Tokyo.", "main".</p>
+    </main>
+  );
+};
 
 export default MainContent;
