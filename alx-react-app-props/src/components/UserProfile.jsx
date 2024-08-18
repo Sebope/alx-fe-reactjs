e4bb616c21e55845b0050e66ccc351954d30171d
rosemary ledwaba <rosemaryledwaba@gmail.com>
2024-08-18 22:32:10 +0200

    Modified the app react props

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,24 @@
-function UserProfile(props) {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  }
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
 
-  export default UserProfile;
+function UserProfile() {
+    const userData = useContext(UserContext);
+
+    if (!userData) {
+        return <div>No user data available</div>;
+    }
+
+    return (
+        <div style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
+            <h2>{userData.name}</h2>
+            <p>Email: {userData.email}</p>
+        </div>
+    );
+}
+
+export default UserProfile;
