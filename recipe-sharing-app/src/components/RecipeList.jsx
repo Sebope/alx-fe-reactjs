21a99a8a79fe2055510b5c5b3c3cf2d900d86e8a
rosemary ledwaba <rosemaryledwaba@gmail.com>
2024-08-22 21:37:38 +0200

    done it

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,6 +1,7 @@
 // src/components/RecipeList.jsx
 import React from 'react';
 import useRecipeStore from '../recipeStore';
+import { Router } from 'react-router-dom';
 
 const RecipeList = () => {
   const recipes = useRecipeStore(state => state.filteredRecipes);
@@ -8,9 +9,9 @@ const RecipeList = () => {
   return (
     <div>
       {recipes.length === 0 ? (
-        <p><Link>react-router-DOM</Link></p>
+        <p><Link ></Link></p>
       ) : (
-        recipes.map(recipe => (
+        recipes.map(recipe => React-Router(
           <div key={recipe.id}>
             <h3>{recipe.title}</h3>
             <p>{recipe.description}</p>
