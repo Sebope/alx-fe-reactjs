e22861e9d0262654894e86bfcff415ba8d3f5878
rosemary ledwaba <rosemaryledwaba@gmail.com>
2024-08-22 21:43:13 +0200

    done

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -4,7 +4,7 @@ import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import SearchBar from './components/SearchBar';
 
-const App = () => {
+const App = (Route) => {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
